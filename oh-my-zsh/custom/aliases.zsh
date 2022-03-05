aa79fe64fc96ed1567fb322b8f8d34a464885343
hoony <hoonyland.newsletter@gmail.com>
2022-03-05 10:52:02 +0900

    init

---
+++ oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,17 @@
+alias ibrew='arch -x86_64 /usr/local/bin/brew'
+alias mbrew='arch -arm64e /opt/homebrew/bin/brew'
+
+alias k='kubectl'
+alias ls='exa'
+
+alias gp='git push'
+alias gs='git switch'
+
+alias tf='terraform'
+
+alias ls='exa'
+alias cd-df-node-api='$HOME/Projects/df/node-api'
+
+
+alias aws-profile-clear="export AWS_ACCESS_KEY_ID= && export AWS_SECRET_ACCESS_KEY="
+alias python='python3.7'
